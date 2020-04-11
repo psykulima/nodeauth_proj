a00e19d09c84f8d76c27ba239f0f841494f80ffc
Lore <56326123+psykulima@users.noreply.github.com>
2020-04-11 13:45:19 -0400

    new pages
    chaged expressValidator to v5.3.1, added register and login page.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest:'./uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET users Register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', {title:'Register'});
+});
+
+/* GET users Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', {title:'Login'});
+});
+
+
+/* GET users Register page. */
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.email);
+});
+
+
+
+
+/* GET users Logout page. 
+router.get('/logout', function(req, res, next) {
+  res.render('logout', {title:'Logout'});
+});
+*/
 
 module.exports = router;
