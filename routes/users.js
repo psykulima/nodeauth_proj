dae6827083fc3757c64fa47c98e5bf6f3306744c
Lore <56326123+psykulima@users.noreply.github.com>
2020-04-15 17:47:04 -0400

    updates
    updates

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest:'./uploads'});
 
+var User= require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -19,9 +21,55 @@ router.get('/login', function(req, res, next) {
 });
 
 
-/* GET users Register page. */
+/* POST users Register page. */
 router.post('/register', upload.single('profileimage'), function(req, res, next) {
-  console.log(req.body.email);
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    if(req.file){
+        console.log('uploading file...');
+        var profileimage = req.file.filename;
+    }else {
+        console.log('no file upload..');
+        var profileimage = 'noimage.jpg';
+    }
+    
+    //Form Validator
+    req.checkBody('name','Name field is required').notEmpty();
+    req.checkBody('email','Email field is required').notEmpty();
+    req.checkBody('email','Email not valid').isEmail();
+    req.checkBody('username','Username field is required').notEmpty();
+    req.checkBody('password','Password field is required').notEmpty();
+    req.checkBody('password2','Password do not match').equals(req.body.password);
+    
+    //Check Error
+    var errors = req.validationErrors();
+    if(errors){
+        console.log('errors');
+        res.render('register',{
+            errors:errors
+        });
+    }else{
+        console.log('no errors');
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password,
+            profileimage: profileimage
+        });
+        
+        User.createUser(newUser, function(err,user){
+            if(err)throw err;
+            console.log(user);
+        });
+        
+        res.location('/');
+        res.redirect('/');
+    }
 });
 
 
